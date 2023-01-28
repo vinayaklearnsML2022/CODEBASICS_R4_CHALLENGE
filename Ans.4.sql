059ea8fae5ad3a64edcfc5aa89c3e0d04ad5e7f4
vinayaklearnsML2022 <104782886+vinayaklearnsML2022@users.noreply.github.com>
2023-01-28 10:34:27 +0530

    Updated Ans4.sql - Added Distinct

--- Ans.4.sql
+++ Ans.4.sql
@@ -1,6 +1,6 @@
 
 WITH cte1 as(
-SELECT dp.segment, COUNT(dp.product_code) as product_count_2020 FROM dim_product dp
+SELECT dp.segment, COUNT(DISTINCT dp.product_code) as product_count_2020 FROM dim_product dp
 JOIN fact_sales_monthly fsm
 ON fsm.product_code=dp.product_code
 WHERE fsm.fiscal_year=2020
@@ -8,7 +8,7 @@ GROUP BY segment
 ORDER BY product_count_2020 DESC),
 
 cte2 as(
-SELECT dp.segment, COUNT(dp.product_code) as product_count_2021 FROM dim_product dp
+SELECT dp.segment,  COUNT(DISTINCT dp.product_code) as product_count_2021 FROM dim_product dp
 JOIN fact_sales_monthly fsm
 ON fsm.product_code=dp.product_code
 WHERE fsm.fiscal_year=2021
@@ -20,7 +20,6 @@ FROM cte1 c1
 JOIN cte2 c2
 ON c1.segment=c2.segment
 ORDER BY difference DESC
-
 
 
 
